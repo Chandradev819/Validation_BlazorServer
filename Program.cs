d130cb196839b86671e4aab985b207e840779959
Chandradev Sah <chandradev819@hotmail.com>
2024-02-29 12:54:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Validation_BlazorServer.Components;
+using FluentValidation;
+using Validation_BlazorServer.Model;
+
+namespace Validation_BlazorServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+            builder.Services.AddTransient<IValidator<Person>, PersonValidator>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
